fc0d395308fb93e36a8a6fd4d2c8846b3f6011d9
Gergely Kamarás <gergely.kamaras@gmail.com>
2023-04-06 14:05:37 +0200

    Add: basic map display

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    useEffect( () => {
 
-  return (
-    <div className="App">
-        <h1>Map demo application</h1>
-        <div id="map"></div>
-    </div>
-  )
+        // Initialization issue solved via: https://stackoverflow.com/questions/71809240/uncaught-error-map-container-is-already-initialized
+        var container = L.DomUtil.get("map");
+
+        if (container != null) {
+        container._leaflet_id = null;
+        }
+
+        var map = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    }, [])
+
+    return (
+        <div className="App">
+            <h1>Map demo application</h1>
+            <div id="map"></div>
+        </div>
+    )
 }
 
 export default App
